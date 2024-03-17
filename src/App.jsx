fec2b2f357015a1b6a7e700f6338bffc77089fd1
harshedabdulla <harshed2004@gmail.com>
2024-03-17 13:57:23 +0530

    hourly stats

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import Monthly from './pages/Monthly'
 import Daily from './pages/Daily'
+import Hourly from './pages/Hourly'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Monthly />} />
         <Route path="/daily" element={<Daily />} />
+        <Route path="/hourly" element={<Hourly />} />
       </Routes>
     </BrowserRouter>
   )
